ae9fc86b0e8f41383cad433c6ea0c7e717417094
Well <welliaquimquim@gmail.com>
2025-04-25 03:47:37 -0400

    feat: update a few style effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col justify-around bg-gray-950 p-4">
+    <main className="flex min-h-screen flex-col justify-around bg-gray-950 p-4">
       <div className="flex flex-col items-center gap-5">
         <h1 className="justify-center p-2 text-center text-2xl text-pretty">
           Com quem você quer dividir os registros de um dia tão especial?
@@ -16,7 +16,7 @@ export default function Home() {
           Criar um álbum
         </Link>
       </div>
-      <div className="mb-12 flex flex-col items-center">
+      <div className="flex flex-col items-center">
         <span className="text-gray-500">Já possui um QR Code?</span>
         <Link
           href="/scanqrcode"
